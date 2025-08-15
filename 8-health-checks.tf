03019b027f62d2c9c391345b1d3a6fc081d04eb9
NichFos <nicholasdfoster645@gmail.com>
2025-08-15 18:12:40 -0500

    Added regions back

--- 8-health-checks.tf
+++ 8-health-checks.tf
@@ -1,63 +1,70 @@
 resource "google_compute_region_health_check" "jourdan-hc" {
-  name                = "jourdan-hc"
-  timeout_sec        = 1
-  check_interval_sec = 1
+  name               = "jourdan-hc"
+  region             = "us-central1"
+  timeout_sec        = 5
+  check_interval_sec = 5
   http_health_check {
-    port         = 80
+    port = 80
   }
 }
 
 resource "google_compute_region_health_check" "vito-hc" {
-  name                = "vito-hc"
-  timeout_sec        = 1
-  check_interval_sec = 1
+  name               = "vito-hc"
+  region             = "southamerica-east1"
+  timeout_sec        = 5
+  check_interval_sec = 5
   http_health_check {
-    port         = 80
+    port = 80
   }
 }
 
 resource "google_compute_region_health_check" "joshua-hc" {
-  name                = "joshua-hc"
-  timeout_sec        = 1
-  check_interval_sec = 1
+  name               = "joshua-hc"
+  region             = "asia-northeast1"
+  timeout_sec        = 5
+  check_interval_sec = 5
   http_health_check {
-    port         = 80
+    port = 80
   }
 }
 
 resource "google_compute_region_health_check" "nick-hc" {
-  name                = "nick-hc"
-  timeout_sec        = 1
-  check_interval_sec = 1
+  name               = "nick-hc"
+  region             = "europe-west2"
+  timeout_sec        = 5
+  check_interval_sec = 5
   http_health_check {
-    port         = 80
+    port = 80
   }
 }
 
 resource "google_compute_region_health_check" "xavier-hc" {
-  name                = "xavier-hc"
-  timeout_sec        = 1
-  check_interval_sec = 1
+  name               = "xavier-hc"
+  region             = "us-west1"
+  timeout_sec        = 5
+  check_interval_sec = 5
   http_health_check {
-    port         = 80
+    port = 80
   }
 }
 
 resource "google_compute_region_health_check" "law-hc" {
-  name                = "law-hc"
-  timeout_sec        = 1
-  check_interval_sec = 1
+  name               = "law-hc"
+  region             = "us-east1"
+  timeout_sec        = 5
+  check_interval_sec = 5
   http_health_check {
-    port         = 80
+    port = 80
   }
 }
 
 resource "google_compute_region_health_check" "yashua-hc" {
-  name                = "yashua-hc"
-  timeout_sec        = 1
-  check_interval_sec = 1
+  name               = "yashua-hc"
+  region             = "asia-east2"
+  timeout_sec        = 5
+  check_interval_sec = 5
   http_health_check {
-    port         = 80
+    port = 80
   }
 }
 
